f42a06a0209798c54b28a8bedf6d24b5f41fca86
Urmas Lamster <urmasl@gmail.com>
2014-08-08 22:05:09 +0300

    forgot to send the latest legbot.js

--- legbot.js
+++ legbot.js
@@ -1,11 +1,18 @@
+"use strict";
+
+var log = require('./log.js');
+
+log.log("Starting legbot");
 
 var client = require('./client.js');
-
 var Channel = require('./Lib/Channel.js');
 
+//This is a dictionary of channel objects.
 var channels = {};
 
+var tc;
+
 client.client.on('connect', function(){
-	console.log("Connection!");
+	log.log("Connected!");
 	tc = new Channel("#every_day_is_leg_day");
 });
